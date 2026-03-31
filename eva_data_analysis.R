45eb69779e348a966d2e73e438b29807ff685a47
Afees Ajasa <afeezajasa@gmail.com>
2026-03-31 12:57:49 -0500

    made code more readable

--- eva_data_analysis.R
+++ eva_data_analysis.R
@@ -1,75 +1,50 @@
-# https://data.nasa.gov/resource/eva.json (with modifications)
-input_file = 'eva_data.json'
-output_file = 'eva_data.csv'
-graph_file = 'comulative_eva_graph.png'
-
+library(tidyverse)
 library(jsonlite)
 library(lubridate)
-library(tidyverse)
 
-j_l <- read_json(input_file)
-data=as.data.frame(j_l[[1]])
+# Files
+input_file  <- "eva_data.json"
+output_file <- "eva_data.csv"
+graph_file  <- "cumulative_eva_graph.png"
 
-for( i in 2:374){
-  r = j_l[[i]]
-    print(r)
-    data =merge(data, as.data.frame(r),  all=TRUE)
-}
-#data.pop(0)
-## Comment out this bit if you don'duration_dt want the spreadsheet
-write.csv(output_file)
+# 1) Read JSON array into a tibble
+eva_tbl <- jsonlite::fromJSON(input_file) |>
+  as_tibble()
 
+# 2) Convert types + drop missing duration/date
+eva_tbl <- eva_tbl |>
+  mutate(
+    eva  = as.numeric(eva),
+    date = ymd_hms(date, quiet = TRUE)
+  ) |>
+  filter(!is.na(duration), duration != "", !is.na(date))
 
+# 3) Write CSV (index=False equivalent)
+readr::write_csv(eva_tbl, output_file)
 
-time <- c()
-date = Date()
+# 4) Sort by date
+eva_tbl <- eva_tbl |>
+  arrange(date)
 
+# 5) duration_hours + cumulative_time
+eva_tbl <- eva_tbl |>
+  mutate(
+    duration_hours = {
+      parts <- str_split(duration, ":", n = 2, simplify = TRUE)
+      as.numeric(parts[, 1]) + as.numeric(parts[, 2]) / 60
+    },
+    cumulative_time = cumsum(duration_hours)
+  )
 
-j=1
-for (i in rownames(data)){
-    print(data[j, ])
-    # and this bit
-    # w.writerow(data[j].values())
-    if (!is.na(data[j,]$duration)){
-        duration_str=data[j,]$duration
-        if(duration_str == ''){
-          #do nothing
-        }else{
-            duration_dt=as.POSIXlt(duration_str,format='%H:%M')
-            duration_hours <- as.numeric(as.difftime(hour(duration_dt), units = 'hours')+as.difftime(minute(duration_dt), units='mins')+as.difftime(second(duration_dt), units='secs'))/(60*60)
-            print(duration_dt,duration_hours)
-            time <- c(time, duration_hours)
-            if(!is.na(data[j,]$date)){
-                date= c(date, as.Date(substr(data[j,'date'], 1, 10), format = '%Y-%m-%d'))
-                #date.append(data[j]['date'][0:10])
-
-            }else{
-              time <- time[1:length(time) -1]
-                }
-            }
-        }
-    j = j+1
-}
-
-duration_dt=0
-for(i in time)
-    duration_dt <- c(duration_dt, duration_dt[length(duration_dt)]+i)
-
-
-df <- data.frame(
-date, time
-)[order(date, time), ]
-
-date <- df$date
-time <- df$time
-cummulative_time = duration_dt[2:length(duration_dt)]
-
-p <- ggplot(df, aes(x = date, y = cummulative_time)) +  
+# 6) Plot + save
+p <- ggplot(eva_tbl, aes(x = date, y = cumulative_time)) +
   geom_point() +
-  geom_line() +  
-  labs(x = "Year", y = "Total time spent in space to date (hours)") +
+  geom_line() +
+  labs(
+    x = "Year",
+    y = "Total time spent in space to date (hours)"
+  ) +
   theme_minimal()
 
 ggsave(graph_file, plot = p, width = 9, height = 5, dpi = 300)
-
 print(p)
